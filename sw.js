90195e79c20b4deb2d69f23402ba6e84f02174b0
olalesolis <olalesolis@gmail.com>
2026-09-11 00:42:00 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const C="te-regalo-v3";
+const C="te-regalo-v4";
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll([
-  "./","./index.html","./manifest.json","./icons/apple-touch-icon.png","./icons/icon-192.png","./icons/icon-512.png"
+  "./","./index.html","./manifest.json","./apple-touch-icon.png","./icon-192.png","./icon-512.png"
 ]))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
