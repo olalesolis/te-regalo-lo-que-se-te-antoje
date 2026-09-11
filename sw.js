5c391cc116730f7ae4f67f9be1ce8450dec0dde9
olalesolis <olalesolis@gmail.com>
2026-09-11 00:33:43 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const C="te-regalo-v2";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="te-regalo-v3";
+self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll([
+  "./","./index.html","./manifest.json","./icons/apple-touch-icon.png","./icons/icon-192.png","./icons/icon-512.png"
+]))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
